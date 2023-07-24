632637d88f7040b873e836878a26fba1023b5796
Herbert Mbangzieu <herbertmbangzieu@gmail.com>
2023-07-24 16:07:00 +0100

    initial commit bootstrap project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <div class="w-full h-screen flex justify-center items-center">
+        <h1 class="text-4xl text-center text-green-700">Course Management</h1>
+    </div>
+</body>
+</html>
